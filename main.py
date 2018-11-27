7e6d1c3fa8cd35b73bccf21f292d136176c362cd
Anderson Sanchez <andersonsanchez53@gmail.com>
2018-11-27 13:09:20 -0700

    Fix bug where window wouldn't pop to top if on MacOS

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from utils.window.window import MainApplication
 import tkinter as tk
+import os
+from sys import platform
+
 
 def main():
     root = tk.Tk()
     root.geometry('800x600')
     MainApplication(root).pack(side='top', fill='both', expand=True)
+    if platform == 'darwin':
+        os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
     root.mainloop()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- utils/window/Menu.py
+++ utils/window/Menu.py
@@ -5,7 +5,6 @@ from utils.file_system.scan_directory import scan_directory
 class MenuBar(tk.Menu):
     def __init__(self, parent):
         tk.Menu.__init__(self, parent)
-        self.root = parent
         self.menu_file = tk.Menu(parent, tearoff=False)
         self.menu_file.label = 'File'
         self.menu_file.add_command(label='Search', command=lambda: scan_directory(parent))
