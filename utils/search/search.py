a480506af2da1ec48823fad2f137332e2a7467f2
Anderson Sanchez <andersonsanchez53@gmail.com>
2018-12-31 23:25:08 -0700

    clean up logic

--- utils/search/search.py
+++ utils/search/search.py
@@ -1,5 +1,7 @@
 import requests
+import re
 import os
+import datetime
 from utils.database.does_exist import does_exist
 from utils.search.get_as_base_64 import get_as_base_64
 
@@ -11,6 +13,17 @@ def search(api_key, file, conn):
     # build the query for the api
     name = os.path.basename(file[:-4])
     query = api_key + '&query=' + name.replace(" ", "+")
+
+    # init year var
+    year = int()
+
+    # try to extract year from name
+    for n in name.split():
+        temp = str(re.sub(r"\D", "", n))
+        if temp.isdigit() and 1890 <= int(temp) <= datetime.datetime.now().year:
+            name = name.replace(temp, "")
+            year = temp
+
     try:
         response = requests.get(query).json()
         movie_id = response['results'][0]['id']
@@ -21,12 +34,17 @@ def search(api_key, file, conn):
         path = file
         movie = (movie_id, name, description, release_date, path, get_as_base_64(image))
         temp = requests.get(image).content
-        with open("resources/" + name + ".jpg", "wb") as f:
+
+        if not os.path.exists('movie_posters'):
+            os.mkdir('movie_posters')
+
+        with open("movie_posters/" + name + ".jpg", "wb") as f:
             f.write(temp)
         if does_exist(conn, movie_id):
             return 1
         else:
             return movie
+
     except IndexError:
         print(name)
         return -1
